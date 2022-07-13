bcc15a9269f7769cc5c455f30dca9dd1fcfb02cd
kanishka8276 <kanishkav8jan@gmail.com>
2022-07-13 17:32:29 +0530

    patient list

--- src/Dashboard/DoctorDashboard.js
+++ src/Dashboard/DoctorDashboard.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import { Link} from 'react-router-dom';
+import PatientListItem from './PatientListItem';
 
 function DoctorDashboard() { 
-    return (<></>);
+    return ( 
+    <div className="container-fluid">
+        <div className="row mt-5">
+            <div className="col-12 col-lg-10">
+            <h5>Your Patients</h5>
+        <PatientListItem/>
+        <PatientListItem/>
+        </div>
+        </div>  
+    </div>);
 }
 export default DoctorDashboard;
