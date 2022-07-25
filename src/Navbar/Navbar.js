841ff9a1c53c8f16dd0514ab15ab492652b94ae4
kanishka8276 <kanishkav8jan@gmail.com>
2022-07-25 17:04:19 +0530

    sign out

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,38 +1,37 @@
 import React from 'react';
-import nonotification from '../Asset/nonotification.png'; 
+import {useLocation} from 'react-router-dom';
+import { useAuth } from "../Context/AuthContext";
+import { useNavigate} from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './Navbar.css';
 
 function NavbarContainer() { 
+  const location = useLocation();
+  const {setCurrentUser,profile} = useAuth();
+  const navigate = useNavigate();
     return (
-   
+      <>
+      { location.pathname !== '/' && location.pathname !=='/signUp' && location.pathname !=='/auth/*' && (
     <Navbar collapseOnSelect className="nav-color" fixed="top" variant="dark">
       <Container>
-        <Navbar.Brand href="/dashboardPatient">Home</Navbar.Brand>
+        <Navbar.Brand href={`/dashboard${profile.type}`}>Home</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-          <Nav.Link href="/device">Register device</Nav.Link>
+          
+            {profile.type === "Patient" && (<Nav className="me-auto"><Nav.Link href="/device">Register device</Nav.Link>
             <Nav.Link href="/pastReports">Past Reports</Nav.Link>
             <Nav.Link href="/caretaker">Caretaker</Nav.Link>
-          </Nav>
-          <Nav.Link>
-                <img
-                        src={nonotification}
-                        alt="notifications"
-                        className="navbar-icons"
-                        style={{ height: '20px'}} 
-                />
-          </Nav.Link>
-          <Nav >
-            <Nav.Link href="/signOut">Sign Out</Nav.Link>
-          </Nav>
+          </Nav>)}
         </Navbar.Collapse>
+        <div className="float-end">
+            <button className="btn" onClick={() => {setCurrentUser();navigate('/')}}>Sign Out</button>
+          </div>
       </Container>
       
-    </Navbar>
+    </Navbar>)}
+    </>
     );
 }
 export default NavbarContainer;
