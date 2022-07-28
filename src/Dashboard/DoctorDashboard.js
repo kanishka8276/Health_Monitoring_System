b3d000e9b94b24bc3a0f9b295216fd4a158081c3
Anjali041 <anjali041kumari@gmail.com>
2022-07-28 19:16:03 +0530

    changes

--- src/Dashboard/DoctorDashboard.js
+++ src/Dashboard/DoctorDashboard.js
@@ -1,11 +1,64 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { Link} from 'react-router-dom';
 import { Modal } from 'react-bootstrap';
 import PatientListItem from './PatientListItem';
-import add from '../Asset/add.png'
+import add from '../Asset/add.png';
+import { useAuth } from "../Context/AuthContext";
+import { doc, getDoc ,updateDoc} from "firebase/firestore";
+import { db } from "../firebase"
 
-function DoctorDashboard() { 
+function DoctorDashboard() {
     const [show, setShow] = useState(false);
+    const [email, setEmail] = useState('');
+    const { currentUser} = useAuth();
+    const [patients,setPatients] = useState({});
+    async function addInBackend(data) {
+      console.log(currentUser);
+      const docRef = doc(db, "myPatients",currentUser.uid);
+      const Data={
+      [email]:data
+      };
+     await updateDoc(docRef, Data)
+      .then(docRef => {
+          console.log("A New Document Field has been added to an existing document");
+          getPatient();
+          setShow(false);
+      })
+      .catch(error => {
+          console.log(error);
+      })
+      
+  }
+   async function addPatient() {
+      const docRef = doc(db, "profile", email);
+    try {
+      const docSnap =await getDoc(docRef);
+      if(docSnap.exists()) {
+          console.log(docSnap.data());
+          addInBackend(docSnap.data())
+      } else {
+          console.log("Document does not exist")
+      }
+    
+  } catch(error) {
+      console.log(error)
+  }}
+  async function getPatient() {
+    const docRef = doc(db, "myPatients", currentUser.uid);
+  try {
+    const docSnap =await getDoc(docRef);
+    if(docSnap.exists()) {
+        setPatients(docSnap.data());
+    } else {
+        console.log("Document does not exist")
+    }
+  
+} catch(error) {
+    console.log(error)
+}}
+useEffect(() => {
+  getPatient();
+},[]);
     return ( 
     <div className="container-fluid">
         <div className="row mt-5">
@@ -16,8 +69,8 @@ function DoctorDashboard() {
             {' '}
             Add Patient
         </button></h4>
-        <PatientListItem name="Kanishka" age="21" gender="Female"/>
-        <PatientListItem  name="Piyush" age="22" gender="Male"/>
+        
+        { Object.entries(patients).map(([key, value]) =>  <PatientListItem name={value.name} age={value.age} gender={value.gender}/>) }
         <Modal
           show={show}
           onHide={() => setShow(false)}
@@ -32,13 +85,17 @@ function DoctorDashboard() {
             <button type="button" className="btn-close" onClick={() => setShow(false)} />
           </Modal.Header>
           <Modal.Body className="bg-modal">
-            <input type="text" placeholder='Patient id'  className="form-control"/>
+            <input type="text" placeholder='Patient id'  className="form-control"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}/>
           </Modal.Body>
           <Modal.Footer>
           <button
               type="button"
               className="btn purple-btn"
-              onClick={() => setShow(false)}
+              onClick={() => addPatient()}
             >
                 Submit</button>  
         </Modal.Footer>
